9faebb7d6026be503f0ce562efb7d69de9310abf
josht007 <148504666+josht007@users.noreply.github.com.>
2025-05-25 15:33:05 -0400

    Correct functionality

---
+++ src/main/java/com/josh007/uploadingfiles/StorageException.java
@@ -0,0 +1,13 @@
+package com.josh007.uploadingfiles;
+
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/josh007/uploadingfiles/UploadingFilesApplication.java
@@ -0,0 +1,28 @@
+package com.josh007.uploadingfiles;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.josh007.uploadingfiles.StorageProperties;
+import com.josh007.uploadingfiles.StorageService;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadingFilesApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+}
